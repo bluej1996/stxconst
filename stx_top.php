ac57acb6de5a22b46947928df0122a28b1d8396f
bluej1996 <bluej1996@gmail.com>
2022-03-21 13:17:14 +0900

    최초 버전 발행

---
+++ stx_top.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" type="image/x-icon" href="stx_images/favicon.png">
+
+    <link rel="apple-touch-icon-precomposed" href="stx_images/mobile_web_icon.png">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+    <title>STX 건설</title>
+    <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+        <![endif]-->
+    <script src="../stx_js/prefixfree.min.js"></script>
+    <script src="../stx_js/respond.min.js"></script>
+    <link rel="stylesheet" href="../stx_css/reset.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="../stx_js/script.js"></script>
+    <script src="../stx_js/part.js"></script>
+    <link rel="stylesheet" href="../stx_css/style.css">
+</head>
+
+<body>
+    <div class="wrap">
+        <!-- 상단 -->
+        <header class="header">
+            <div class="container">
+                
+                <a href="http://bluej1996.dothome.co.kr" title="STX 건설" class="logo">
+                    <img src="../stx_images/logo.png" alt="STX 건설">
+                </a>
+
+                <div class="language">
+                    <ul class="language-list">
+                        <li>
+                            <a href="#" class="language-no">eng</a>
+                        </li>
+                        <li>
+                            <a href="#">kor</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <nav class="nav">
+                <div class="container">
+                    <ul class="gnb">
+                        <li>
+                            <a href="#">회사소개</a>
+                            <ul class="submenu">
+                                <li><a href="#">인사말</a></li>
+                                <li><a href="#">STX건설</a></li>
+                                <li><a href="#">기업문화</a></li>
+                                <li><a href="#">STX건설연혁</a></li>
+                                <li><a href="#">조직안내</a></li>
+                                <li><a href="#">윤리경영</a></li>
+                                <li><a href="#">안전·환경·품질경영</a></li>
+                                <li><a href="#">찾아오시는 길</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">사업분야</a>
+                            <ul class="submenu">
+                                <li><a href="#">건축사업</a></li>
+                                <li><a href="#">주택사업</a></li>
+                                <li><a href="#">토목사업</a></li>
+                                <li><a href="#">플랜트/공작기계사업</a></li>
+                                <li><a href="#">해외사업</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">사회공헌</a>
+                            <ul class="submenu">
+                                <li><a href="#">나눔의생각</a></li>
+                                <li><a href="#">주요활동분야</a></li>
+                                <li><a href="#">활동현황</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">홍보센터</a>
+                            <ul class="submenu">
+                                <li><a href="#">홍보동영상</a></li>
+                                <li><a href="#">홍보브로슈어</a></li>
+                                <li><a href="#">STX건설뉴스</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="http://bluej1996.dothome.co.kr/bbs/board.php?bo_table=test">고객지원</a>
+                            <ul class="submenu">
+                                <li><a href="#">자주묻는질문</a></li>
+                                <li><a href="#">고객문의</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">채용정보</a>
+                            <ul class="submenu">
+                                <li><a href="#">채용안내</a></li>
+                                <li><a href="#">채용공고</a></li>
+                                <li><a href="#">채용FAQ</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <!--// 상단 -->
+        <!-- 메인 -->
+        <div class="main">
+            <div class="container">
